6711bcc1e60e0bccff2ee8e8073ac8d40942b622
Tai Zhang <tzhan329@uwo.ca>
2017-02-06 16:47:33 +0000

    Lab2.2

--- Lab2-p1/script.js
+++ Lab2-p1/script.js
@@ -1,10 +1,12 @@
 // JavaScript File
 
-
+var c = 0;
+var basket=[];
+var fruits = ["apple","banana", "orange", "mango"];
 
 function listfruit(){ 
-    var fruits, list, fLen, i;
-    fruits = ["apple","banana", "orange", "mango"];
+    var list, fLen, i;
+
     fLen = fruits.length;
     list = "<ul>";
     for (i = 0; i < fLen; i++) {
@@ -15,24 +17,45 @@ function listfruit(){
 }
 
 function myFunction() {
-    var x, text, basket, baslist;
+    var x, text, baslist, basfull, i, j;
 
     // Get the value of the input field with id="numb"
     x = document.getElementById("numb").value;
 
     // If x is Not a Number or less than one or greater than 10
     if (x =="apple" || x =="banana" || x =="orange" || x =="mango") {
-        text = "OK"
-        basket = [];
+        text = "OK";
         
+        if( c > 3){ 
+            basfull="basket is full";
+            document.getElementById("demo3").innerHTML = basfull;
+        }else{
+            for (i=0; i<=c; i++){
+                if(x==basket[i]){
+                    basfull="you have already picked this";
+                    break;
+                }
+            }
+                if(i>c){
+                    basket[c] = x;
+                    c++;
+                    baslist = "<ul>";
+                    for (j = 0; j <c ; j++) {
+                        baslist += "<li>" + basket[j] + "</li>";
+                    }
+                    baslist += "</ul>";
+                    document.getElementById("demo3").innerHTML = baslist;                     
+                }
+               
+         }
     } else {
         text = "Input not valid";
-    }
+     }
     document.getElementById("demo2").innerHTML = text;
-    return baslist;
+
 }
     
-function findfruit() {
+function showbaslist(){
    document.getElementById("p2").innerHTML = "Paragraph changed.";
 }
 
